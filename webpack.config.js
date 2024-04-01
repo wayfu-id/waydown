979fcf9b044f20d477cf09526df0b5c2d761323d
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2024-04-01 23:12:22 +0700

    chore(build): Set umdNamedDefine true for webpack builder

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ export default {
             name: "Waydown",
             type: "umd",
             export: "default",
+            umdNamedDefine: true,
         },
     },
     devtool: "source-map",
